bb382352219a6be36cc623bd15a50bb63a3d93b5
QuinceyNiu <490332924@qq.com>
2020-11-16 23:06:18 +0800

    1010

---
+++ BASIC/1010.cpp
@@ -0,0 +1,19 @@
+//https://pintia.cn/problem-sets/994805260223102976/problems/994805313708867584
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, flag=0;
+    while(cin >> a >> b) {
+        if(b != 0) {
+            if(flag == 1)
+                cout << " ";
+            cout << a*b << " " << b-1;
+            flag = 1;
+        }
+    }
+    if(flag == 0)
+            cout << "0 0";
+    return 0;
+}
